3fb69b5693f01c8e85e6a70dccb73ba0d8a080bc
Prasenna Venkatesh Vepambedu <prasennavenkatesh@gmail.com>
2018-09-30 19:56:01 -0400

    Skeleton setup done

---
+++ MenuManager/src/main/java/com/menumanager/MenuManagerApplication.java
@@ -0,0 +1,12 @@
+package com.menumanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuManagerApplication.class, args);
+	}
+}
